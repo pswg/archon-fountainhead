a8a1a92bf2eb5b5c6d4a237451458179b54f107b
Patrick Kohler <patrick.kohler@gmail.com>
2017-12-31 16:52:50 -0600

    Replaced`git-utils` package with GitCLI.

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
+const { execSync } = require('child_process');
 const cors = require('cors');
 const express = require('express');
 const app = express();
-const git = require('git-utils');
-
-const _any = o => { for(const k in o) if(o.hasOwnProperty(k)) return true; return false; };
 
 // check for updates
 app.use("/", (req, res, next) => {
-    const repo = git.open(__dirname);
-    try {
-        const stat = repo.getStatus();
-        if (_any(stat)) {
-            res.sendStatus(503);
-            server.close();
-        } else {
-            next();
-        }
-    } finally {
-        repo.release();
+    const gitStatus = execSync('git status');
+    if (gitStatus.indexOf('up-to-date') == -1) {
+        res.sendStatus(503);
+        server.close();
     }
 });
 
