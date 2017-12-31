b87ffb5d893a52c12b805e318d9fcc18a690b782
Daniel Scheufler <djscheufler@gmail.com>
2017-12-31 17:33:20 -0600

    feat(Merge): Got meerge view to show and POST gets proper data!

--- app.js
+++ app.js
@@ -4,21 +4,23 @@ const express = require('express');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true })); 
 
+app.get('/merge',function(req,res){
+    res.sendFile('merge.html',{root: __dirname+'/views/'});
+});
+
+app.post('/merge', function(req, res) {
+    res.send('You sent "' + req.body.prNumber + '".');
+});
+
 // check for updates
 app.use("/", (req, res, next) => {
     const gitStatus = execSync('git status');
     if (gitStatus.indexOf('up-to-date') == -1) {
         res.sendStatus(503);
         server.close();
+    } else {
+        next();
     }
-});
-
-app.get('/merge',function(req,res){
-    res.sendFile('views/merge.html');
-});
-
-app.post('/merge', function(req, res) {
-    res.send('You sent "' + req.body.prNumber + '".');
 });
 
 const server = app.listen(3000);
