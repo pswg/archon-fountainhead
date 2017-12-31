1f0770189e3fd3686e0b314ba36ffe33778add0a
Daniel Scheufler <djscheufler@gmail.com>
2017-12-31 12:08:41 -0600

    feat(Merge Page): Add express routing for Merge actions, page for Merging.

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true })); 
+
 const git = require('git-utils');
 
 const _any = o => { for(const k in o) if(o.hasOwnProperty(k)) return true; return false; };
 
 // check for updates
 app.use("/", (req, res, next) => {
-    const repo = git.open(__dirname);
-    try {
-        const stat = repo.getStatus();
-        if (_any(stat)) {
-            res.sendStatus(503);
-            server.close();
-        } else {
-            next();
-        }
-    } finally {
-        repo.release();
-    }
+    // const repo = git.open(__dirname);
+    // try {
+    //     const stat = repo.getStatus();
+    //     if (_any(stat)) {
+    //         res.sendStatus(503);
+    //         server.close();
+    //     } else {
+    //         next();
+    //     }
+    // } finally {
+    //     repo.release();
+    // }
 });
+
+app.get('/merge',function(req,res){
+    res.sendFile('merge.html');
+})
+
+app.post('/merge', function(req, res) {
+    res.send('You sent "' + req.body.prNumber + '".');
+  });
 
 const server = app.listen(3000);
