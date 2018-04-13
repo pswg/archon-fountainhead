eda0e39871187380f153263281b747cfb7450313
Patrick Kohler <patrick.kohler@mp2energy.com>
2018-04-13 09:50:49 -0500

    Rewrote merge function to use request-promise

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+'use strict';
+
+const { exec } = require('child_process');
 const { execSync } = require('child_process');
 const bodyParser = require('body-parser');
 const express = require('express');
+const rp = require('request-promise');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -20,12 +24,30 @@ app.get('/merge', function (req, res) {
 });
 
 app.post('/merge', function (req, res) {
-    const cmd = 'curl https://api.github.com/repos/impyrio/archon-fountainhead/pulls/' +
-        req.body.prNumber +
-        '/merge';
-    const result = execSync(cmd);
-    console.log('Results: ' + result)
-    res.send('Archon merged "' + req.body.prNumber + '". \nResults:\n\t' + result.message);
+    const pr = parseInt(req.body.prNumber);
+    const prUri = `https://api.github.com/repos/impyrio/archon-fountainhead/pulls/${pr}`;
+    const opts = {
+        json: true,
+        headers: { 'User-Agent': 'request' }
+    };
+    rp.get({ uri: prUri, ...opts })
+        .then((json) => {
+            const mergeUri = `${prUri}/merge`;
+            const body = {
+                commit_title : `Merge pull request #${pr} from ${json.head.label}`,
+                commit_message : json.title,
+                sha : json.merge_commit_sha,
+                merge_method: 'merge'
+            };
+
+            return rp.put({uri: mergeUri, body, ...opts})
+                .then(({ message, sha }) => {
+                    res.send(`Result: ${message} \nsha: ${sha}`);
+                });
+        })
+        .catch(({ message, documentation_url }) => {
+            res.send(`Failed: ${message}`);
+        });
 });
 
 const server = app.listen(3000);
